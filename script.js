d333edc7b83ec78f41890472af49c7342344b881
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2021-07-02 13:23:55 +0200

    fished third section

--- script.js
+++ script.js
@@ -40,6 +40,8 @@ var img3 = document.getElementById("img3");
 var img4 = document.getElementById("img4");
 var img5 = document.getElementById("img5");
 var img6 = document.getElementById("img6");
+var img7 = document.getElementById("img7");
+var img8 = document.getElementById("img8");
 
 function closeGallery(){
   img1.style.display = "none";
@@ -48,34 +50,53 @@ function closeGallery(){
   img4.style.display = "none";
   img5.style.display = "none"; 
   img6.style.display = "none";
+  img7.style.display = "none";
+  img8.style.display = "none";
   document.getElementById("galBackgr").style.display = "none";
+  document.getElementById("animMessages").style.marginTop = "50vw";
 }
 
 
 function enlargeImg1() { 
     img1.style.display = "block";     
     document.getElementById("galBackgr").style.display = "block"; 
+    document.getElementById("animMessages").style.marginTop = "65vw";
 }
 function enlargeImg2() { 
-    img2.style.display = "block";  
-    img2.style.width = "25vw"; 
-    img2.style.marginLeft = "20%";  
+    img2.style.display = "block";      
     document.getElementById("galBackgr").style.display = "block"; 
+    document.getElementById("animMessages").style.marginTop = "55vw";
 }
 function enlargeImg3() { 
   img3.style.display = "block";     
   document.getElementById("galBackgr").style.display = "block"; 
+  document.getElementById("animMessages").style.marginTop = "55vw";
 }
 function enlargeImg4() { 
   img4.style.display = "block";     
   document.getElementById("galBackgr").style.display = "block"; 
+  document.getElementById("animMessages").style.marginTop = "55vw";
 }
-function enlargeImg5() { 
+function enlargeImg5() {      
   img5.style.display = "block";     
   document.getElementById("galBackgr").style.display = "block"; 
+  document.getElementById("animMessages").style.marginTop = "65vw";
 }
-function enlargeImg6() { 
+function enlargeImg6() {   
   img6.style.display = "block";     
   document.getElementById("galBackgr").style.display = "block"; 
+  document.getElementById("animMessages").style.marginTop = "65vw";
 }
+function enlargeImg7() {   
+  img7.style.display = "block";     
+  document.getElementById("galBackgr").style.display = "block"; 
+  document.getElementById("animMessages").style.marginTop = "65vw";
+}
+
+function enlargeImg8() {    
+  img8.style.display = "block";     
+  document.getElementById("galBackgr").style.display = "block"; 
+  document.getElementById("animMessages").style.marginTop = "55vw";
+}
+
 
