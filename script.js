dc0e440d7d46e73f58a0d1c2d09503bbd1c7533d
EfiMethodiou <37740639+EfiMethodiou@users.noreply.github.com>
2021-07-04 11:45:40 +0200

    section 4 almost done

--- script.js
+++ script.js
@@ -31,7 +31,6 @@ function showSlides() {
   setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
-
 /*SECOND GALLERY */
 
 var img1 = document.getElementById("img1");
@@ -43,12 +42,12 @@ var img6 = document.getElementById("img6");
 var img7 = document.getElementById("img7");
 var img8 = document.getElementById("img8");
 
-function closeGallery(){
+function closeGallery() {
   img1.style.display = "none";
   img2.style.display = "none";
   img3.style.display = "none";
   img4.style.display = "none";
-  img5.style.display = "none"; 
+  img5.style.display = "none";
   img6.style.display = "none";
   img7.style.display = "none";
   img8.style.display = "none";
@@ -56,47 +55,66 @@ function closeGallery(){
   document.getElementById("animMessages").style.marginTop = "50vw";
 }
 
-
-function enlargeImg1() { 
-    img1.style.display = "block";     
-    document.getElementById("galBackgr").style.display = "block"; 
-    document.getElementById("animMessages").style.marginTop = "65vw";
-}
-function enlargeImg2() { 
-    img2.style.display = "block";      
-    document.getElementById("galBackgr").style.display = "block"; 
-    document.getElementById("animMessages").style.marginTop = "55vw";
-}
-function enlargeImg3() { 
-  img3.style.display = "block";     
-  document.getElementById("galBackgr").style.display = "block"; 
-  document.getElementById("animMessages").style.marginTop = "55vw";
-}
-function enlargeImg4() { 
-  img4.style.display = "block";     
-  document.getElementById("galBackgr").style.display = "block"; 
-  document.getElementById("animMessages").style.marginTop = "55vw";
-}
-function enlargeImg5() {      
-  img5.style.display = "block";     
-  document.getElementById("galBackgr").style.display = "block"; 
+function enlargeImg1() {
+  img1.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
   document.getElementById("animMessages").style.marginTop = "65vw";
 }
-function enlargeImg6() {   
-  img6.style.display = "block";     
-  document.getElementById("galBackgr").style.display = "block"; 
+function enlargeImg2() {
+  img2.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
+  document.getElementById("animMessages").style.marginTop = "55vw";
+}
+function enlargeImg3() {
+  img3.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
+  document.getElementById("animMessages").style.marginTop = "55vw";
+}
+function enlargeImg4() {
+  img4.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
+  document.getElementById("animMessages").style.marginTop = "55vw";
+}
+function enlargeImg5() {
+  img5.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
   document.getElementById("animMessages").style.marginTop = "65vw";
 }
-function enlargeImg7() {   
-  img7.style.display = "block";     
-  document.getElementById("galBackgr").style.display = "block"; 
+function enlargeImg6() {
+  img6.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
+  document.getElementById("animMessages").style.marginTop = "65vw";
+}
+function enlargeImg7() {
+  img7.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
   document.getElementById("animMessages").style.marginTop = "65vw";
 }
 
-function enlargeImg8() {    
-  img8.style.display = "block";     
-  document.getElementById("galBackgr").style.display = "block"; 
+function enlargeImg8() {
+  img8.style.display = "block";
+  document.getElementById("galBackgr").style.display = "block";
   document.getElementById("animMessages").style.marginTop = "55vw";
 }
 
+/*
+4 SECTION MESSAGES ANIMATION
+*/
+function section4Anim() {
+  console.log("hello");
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const square = entry.target.querySelector(".animMessages");
 
+      if (entry.isIntersecting) {
+        square.classList.add("animMessages-animation");
+        return; // if we added the class, exit the function
+      }
+
+      // We're not intersecting, so remove the class!
+      square.classList.remove("animMessages-animation ");
+    });
+  });
+
+  observer.observe(document.querySelector(".animMessages-wrapper"));
+}
